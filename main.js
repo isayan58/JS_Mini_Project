92255bca06b358fdbc53a173a316c404737f2a96
isayan58 <sayan.96.adhikary@gmail.com>
2021-08-30 07:38:36 +0530

    Updated

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let initBook=[
         id: 2,
         name: "Pride and Prejudice",
         author: "Jane Austen",
-        lender: "Priyarna",
+        lender: "XYZ",
         borrower: null
     }
 ];
